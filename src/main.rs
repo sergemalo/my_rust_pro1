d869565515640d292e9b845e68536bbfda6bb93c
Serge Malo <sergemalo2000@outlook.com>
2023-12-27 12:49:01 -0500

    Added println! examples

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,14 @@ My Hello World app"]
 
 fn main() {
 
-#![doc = r"# MAIN function
-This is the doc of my main
-```
-fn main()
-```
-
-Reads user input and spit it our"]
+#![doc = "# MAIN function\nThis is the doc of my main\n```\nfn main()\n```\n\nReads user input and spit it our"]
 
     println!("Hello, world!");
+    println!("{prenom} {Nom}", prenom="Serge", Nom="Malo");
+    // Traits:
+    println!("Dec: {0}, Hex: 0x{0:x}", 42);
+    // Debug Trait
+    println!("DBG: {:?}", [1, 2, 3]);
 
     let mut input = String::new();
     println!("Say something:");
